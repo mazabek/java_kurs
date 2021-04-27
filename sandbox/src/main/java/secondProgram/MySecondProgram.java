093c29a9ab16263be665fe9b7a5ef4b200fb76a8
mazabek <mzabek@sii.pl>
2021-04-27 14:34:53 +0200

    Second program v.2

--- sandbox/src/main/java/secondProgram/MySecondProgram.java
+++ sandbox/src/main/java/secondProgram/MySecondProgram.java
@@ -2,16 +2,11 @@ package secondProgram;
 
 public class MySecondProgram {
     public static void main (String[] args){
-
-        Point a = new Point(1,1);
-        Point b = new Point(2,1);
-
-        System.out.println("Point A= ("+a.coordinateX +","+ a.coordinateY+")");
-        System.out.println("Point B= ("+b.coordinateX +","+ b.coordinateY+")");
-        System.out.println(distance(a,b));
+        Point result = new Point(1,1,1,2);
+        System.out.println("Point X: ("+result.coordinateX1 + ","+result.coordinateY1+")");
+        System.out.println("Point Y: ("+result.coordinateX2 + ","+result.coordinateY2+")");
+        System.out.println("Distance between point X and Y equals: "+result.distance());
     }
-    public static double distance (Point a, Point b){
-        return Math.sqrt(Math.pow((b.coordinateX-a.coordinateX), 2)+Math.pow((b.coordinateY-a.coordinateY), 2));
-    }
+
 
 }
--- sandbox/src/main/java/secondProgram/Point.java
+++ sandbox/src/main/java/secondProgram/Point.java
@@ -2,13 +2,21 @@ package secondProgram;
 
 public class Point {
 
-    public double coordinateX;
-    public double coordinateY;
+    public double coordinateX1;
+    public double coordinateY1;
+    public double coordinateX2;
+    public double coordinateY2;
 
 
-    public Point (double coordinateX, double coordinateY){
-        this.coordinateX = coordinateX;
-        this.coordinateY = coordinateY;
+    public Point (double coordinateX1, double coordinateY1, double coordinateX2, double coordinateY2){
+        this.coordinateX1 = coordinateX1;
+        this.coordinateY1 = coordinateY1;
+        this.coordinateX2 = coordinateX2;
+        this.coordinateY2 = coordinateY2;
+    }
+
+    public double distance (){
+        return Math.sqrt(Math.pow((this.coordinateX2-this.coordinateX1), 2)+Math.pow((this.coordinateY2-this.coordinateY1), 2));
     }
 
 
