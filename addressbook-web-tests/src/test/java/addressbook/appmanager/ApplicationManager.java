5da463a2636a87fec87dc4d18e0594591fc61a90
mazabek <mzabek@sii.pl>
2021-05-07 20:23:49 +0200

    Homework ex.7

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,10 @@ package addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,22 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if (browser.equals(BrowserType.EDGE)){
+            wd = new EdgeDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:1000/addressbook/");
         groupHelper = new GroupHelper(wd);
