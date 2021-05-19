9ebc608a00c077e42ca551658f02621bc4c0bd3c
mazabek <mzabek@sii.pl>
2021-05-19 11:07:46 +0200

    exercises - collection and lists

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
@@ -1,7 +1,10 @@
 package addressbook.tests;
 
 import addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
@@ -11,10 +14,17 @@ public class GroupDeletionTests extends TestBase{
     if (! app.getGroupHelper().isThereAGroup()){
         app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     app.logOut();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
+
   }
 
 }
